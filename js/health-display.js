2e86a3409758db1a5cecb0dda16825b6ffa38e3f
Cesar Vinken <cesarvinken@hotmail.com>
2018-08-17 10:53:19 +0200

    deleted double file

--- js/health-display.js
+++
@@ -1,7 +0,0 @@
-function displayHealth() {
-  let health = "";
-  for (let index = 0; index < sprites.player.health; index++) {
-    health += "<img src='health.jpeg'>";
-  }
-  $("#health-container").html(health);
-}
